9f6675b3e37a11d2fc64b8676901eea04c2787fb
Anders Pearson <anders@columbia.edu>
2012-03-11 22:33:17 -0400

    individual tag pages

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from annoying.decorators import render_to
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
 from faktum.main.models import Fact, Tag, FactTag
 
 @render_to("main/index.html")
 def index(request):
     return dict(facts=Fact.objects.all().order_by("-added")[:20])
+
+@render_to("main/tag.html")
+def tag(request,tag_id):
+    tag = get_object_or_404(Tag,id=tag_id)
+    return dict(tag=tag,facts=[ft.fact for ft in tag.facttag_set.all().order_by("-fact__added")[:20]])
 
 @login_required
 def add(request):
