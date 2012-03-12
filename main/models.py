fa26646b3123138b41469d29557978bd2770ae71
Anders Pearson <anders@columbia.edu>
2012-03-11 21:01:28 -0400

    basic data model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Fact(models.Model):
+    title = models.CharField(max_length=256)
+    details = models.TextField(default="",blank=True,null=True)
+    user = models.ForeignKey(User)
+    source_name = models.CharField(max_length=256,default="")
+    source_url = models.CharField(max_length=256,default="",blank=True,null=True)
+
+
