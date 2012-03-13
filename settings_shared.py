fc587c8382347425fd1418fb60626038d7cef3ab
anders <anders@columbia.edu>
2012-03-13 00:15:47 -0400

    need an activation setting

--- settings_shared.py
+++ settings_shared.py
@@ -97,4 +97,4 @@ WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper','djangowind.auth.Staff
 WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
 WIND_SUPERUSER_MAPPER_GROUPS = ['anp8','jb2410','zm4','sbd12','egr2107','kmh2124','sld2131','amm8','mar227','ed2198', 'ej2223']
 
-
+ACCOUNT_ACTIVATION_DAYS = 7
