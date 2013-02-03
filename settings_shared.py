ec4b7d067d6e635ffa9b4d95e12b1af44970999d
Anders Pearson <anders@columbia.edu>
2013-02-03 14:44:40 -0500

    remove statsd middleware references

--- settings_shared.py
+++ settings_shared.py
@@ -64,8 +64,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     )
 
 MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
